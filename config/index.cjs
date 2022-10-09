478dd732938909f193520d2c8219159072f9214f
Lhoddy <forgithublhd@163.com>
2022-10-09 18:56:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,38 +13,50 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '61a519012920dc9bae9f38defc6562cd',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '湖南',
+  CITY: '望城',
+  
+  SWITCH: {
+    weather: true,
+    holidaytts: true,
 
+    /** 每日N句 */
+    // 金山每日一句
+    CIBA: true,
+    horoscope: ture,
+  },
+  
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '霏霏宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ogag-5hMAKoFuz5ItjGJHCMmxzoQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'aiiGG0PW1j22dFm_QI5Xs_yikCMwFHnvyRuAKoYyR4o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-06',
+      horoscopeDateType: '今日',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
+        /*{
           type: '*生日', name: '宝贝', year: '1996', date: '09-09',
         },
+        */
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '宝贝', year: '1996', date: '09-06',
         },
-        {
+        /*{
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
+        },*/
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2019-07-20' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
