92ced106c7cb5fe4de98b12e2a6d2030a6dab58e
Lhoddy <forgithublhd@163.com>
2022-10-10 08:43:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,7 +31,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '霏霏宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ogag-5hMAKoFuz5ItjGJHCMmxzoQ',
+      id: 'ogag-5k8tjTeCsA_5709waEOLxoQ', //ogag-5k8tjTeCsA_5709waEOLxoQ
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'z6HSDvdqvJRuq-5qvUOq9DfTXwEMel9LWHOnbvS3k9E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
